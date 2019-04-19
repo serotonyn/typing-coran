b0fb4030de64e1d0c486d1a15245cf7ccddfc8e3
seroto <youyou1youcef@gmail.com>
2019-04-19 19:46:08 +0100

    Updates

---
+++ precache-manifest.0cc4d82bae322968f3ee92fbe71e3d23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ad56ee29697d0482a46",
+    "url": "/typing-coran/static/js/runtime~main.3be33c7b.js"
+  },
+  {
+    "revision": "8cc2911c9f713b4cb1ee",
+    "url": "/typing-coran/static/js/main.26b15fd0.chunk.js"
+  },
+  {
+    "revision": "70d03359fc8f85594b14",
+    "url": "/typing-coran/static/js/2.97bcac8e.chunk.js"
+  },
+  {
+    "revision": "8cc2911c9f713b4cb1ee",
+    "url": "/typing-coran/static/css/main.c4721349.chunk.css"
+  },
+  {
+    "revision": "fbeed4a1a603c4546385cebe42a8c031",
+    "url": "/typing-coran/index.html"
+  }
+];
